1982ba1b05e19ef7d9977db6b1527522964fb684
asnamv08 <asnamvarghese001@gmail.com>
2024-02-08 23:35:16 +0530

    view created

--- lib/pages/view.dart
+++ lib/pages/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:patient/models/patientmodel.dart';
 
 class viewpatient extends StatefulWidget {
   const viewpatient({super.key});
@@ -8,10 +9,49 @@ class viewpatient extends StatefulWidget {
 }
 
 class _viewpatientState extends State<viewpatient> {
+  Future<List<Posts>>? data;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.arrow_back),
+        ),
+        body: FutureBuilder(
+            future: data,
+            builder: (context,snapshot){
+              if(snapshot.hasData && snapshot.data!.isNotEmpty){
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder:(value,index){
+                      return Card(
+                        child: Column(
+                          children: [
+                            ListTile(
+                                title:  Text("Name: "+snapshot.data![index].patientname.toString()),
+                                subtitle: Text("Address:"+snapshot.data![index].patientaddress.toString()+"\n"+"Phoner:"+snapshot.data![index].patientphone.toString()+"\n"+"Symptom:"+snapshot.data![index].patientsymptons+"\n"+"Status:"+snapshot.data![index].patientstatus),
+                                leading:CircleAvatar(child: Text(snapshot.data![index].patientname.toString()[0]),)
+                            )
+
+                            /* Text(snapshot.data![index].vistorName.toString()),
+                    Text(snapshot.data![index].vistorPlace.toString()),
+                    Text(snapshot.data![index].vistorAadhar.toString()),
+                    Text(snapshot.data![index].vistorPlace.toString()),
+                    Text(snapshot.data![index].vistorPhone.toString()),*/
+                          ],
+                        ),
+                      );
+
+                    }
+                );
+              }
+              else{
+                return Text("Fetching data");
+              }
+
+            }
+
+        ),
 
       ),
     );
